7a55baf9fd243cbe50612bc6d1392fc4cc03de1d
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2023-04-02 19:49:36 +0530

    components, props, multiple react elements and state

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 // const body = document.querySelector("body");
 // console.log(body);
 
@@ -19,19 +20,105 @@ import ReactDOM from "react-dom/client";
 //   alert("hello world");
 // }
 
-const root = document.getElementById("root");
-console.log(root);
+const span = React.createElement("span");
+span.innerHTML = "Hello";
+
+// console.log(span);
+
+//2 ways of creating React element
 const heading = React.createElement("h1", {
   className: "btn",
-  xyz: "abc",
-  children: "Hello World",
+  children: [React.createElement("span", { children: "Hello" }), "World"],
 });
 
-console.log(heading);
+console.log(heading, "heading");
+
+const heading2 = (
+  <h1 className="btn" xyz="abc">
+    <span>Hello</span>World
+  </h1>
+);
+
+console.log(heading2, "heading2");
+
+//component
+
+// function DisplayHelloWorld(props) {
+//   console.log(props);
+//   return heading2;
+// }
+
+//using props
+function SayGreeting(props) {
+  console.log("hello");
+  let [hobby, setHobby] = useState("cricket");
+  const [count, setCount] = useState(0);
+
+  ////Hoobby: football
+
+  //   function changeHobby() {
+  //     setHobby("Hello world");
+  //   }
+
+  function increaseCount() {
+    let c = count + 1;
+    setCount(c);
+  }
+
+  function decreaseCount() {
+    let c = count - 1;
+    setCount(c);
+  }
+  console.log(count);
+  return (
+    <h1 className="heading" xyz="abc">
+      {/* <span>{props.greeting}</span>
+      {props.name}
+      <p>Your age is : {props.age}</p>
+      <p>Your hobby is : {hobby}</p>
+      <button onClick={changeHobby}>Change Hobby</button> */}
+      <button onClick={increaseCount}>+</button>
+      <span>{count}</span>
+      <button onClick={decreaseCount}>-</button>
+    </h1>
+  );
+}
+
+// display ---> sayGreeting
+
+function DisplayGreeting(props) {
+  if (props.time >= 6) {
+    return (
+      <SayGreeting greeting="Good evening" name={props.name} age={props.age} />
+    );
+    ///Good evening
+  } else {
+    return (
+      <SayGreeting greeting="Good morning" name={props.name} age={props.age} />
+    );
+    //Good Morning
+  }
+}
+
+// before 6 it should give Good morning After 6 it should give me Good evening
+
+const root = document.getElementById("root");
+// console.log(heading);
 const rootElement = ReactDOM.createRoot(root);
-console.log(rootElement);
-rootElement.render(heading);
+// console.log(rootElement);
+rootElement.render(<DisplayGreeting time="1" name="Subham" age="20" />);
 /**
  * 1. Element
  * 2. Root element
+ * 3. props
+ *JSX
+ 4. Component
+ 5.State v.s props
+<div id="root">
+
+<h1><span>Hello</span> World</h1>
+</div>
+1. create a root
+2. create a react element
+ * 
  */
